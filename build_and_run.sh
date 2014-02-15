46e32306033bd687e98c10ff26e3e971ac801572
root <root@6e4efd1a-7b47-4cd7-b6dc-9c5897abb8d9.cs3a9ccloud.internal>
2014-02-15 21:57:40 +0900

    setup complete code

---
+++ build_and_run.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+./build_maven.sh
+if [ $? != 0 ]; then
+	echo "[casspool_tool error build maven]"
+	exit 1;
+fi
+echo "[casspool_tool success build maven]"
+
+PID=`ps -ef | grep casspool | grep -v grep | awk '{print $2}'`
+if [ -n "$PID" ]; then
+	kill $PID
+	echo "[casspool_tool kill casspool process]"
+fi
+
+./run_jar.sh
+
+if [ $? != 0 ]; then
+	exit 1
+fi
+
+exit 0
--- run_jar.sh
+++ run_jar.sh
@@ -1,19 +1,10 @@
 #!/bin/bash
 
-./build_maven.sh
+nohup java -cp target -jar target/casspool.jar 1> ../casspool.log 2>&1 &
 if [ $? != 0 ]; then
-	echo "[casspool_tool error build maven]"
-	exit 1;
+	echo "[casspool_tool fail to run casspool server]"
+	exit 1
 fi
-echo "[casspool_tool success build maven]"
-
-PID=`ps -ef | grep casspool | grep -v grep | awk '{print $2}'`
-if [ -n "$PID" ]; then
-	kill $PID
-	echo "[casspool_tool kill casspool process]"
-fi
-
-java -cp target -jar target/casspool.jar &
 echo "[casspool_tool started casspool server]"
 
 exit 0
