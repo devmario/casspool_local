d3ee3aba2e0a53f5afd81d4bd90d108e7047d63c
root <root@6e4efd1a-7b47-4cd7-b6dc-9c5897abb8d9.cs3a9ccloud.internal>
2014-02-16 23:09:51 +0900

    counter insert

--- src/main/java/com/vanillabreeze/CassInsertMulti.java
+++ src/main/java/com/vanillabreeze/CassInsertMulti.java
@@ -4,23 +4,16 @@ import java.util.*;
 import java.io.*;
 import java.net.*;
 
-import me.prettyprint.cassandra.serializers.StringSerializer;
-import me.prettyprint.cassandra.service.template.ColumnFamilyTemplate;
-import me.prettyprint.cassandra.service.template.ColumnFamilyResult;
-import me.prettyprint.cassandra.service.template.ThriftColumnFamilyTemplate;
-import me.prettyprint.hector.api.Cluster;
-import me.prettyprint.hector.api.Keyspace;
-import me.prettyprint.hector.api.beans.HColumn;
-import me.prettyprint.hector.api.exceptions.HectorException;
-import me.prettyprint.hector.api.factory.HFactory;
-import me.prettyprint.hector.api.mutation.Mutator;
-import me.prettyprint.hector.api.query.ColumnQuery;
-import me.prettyprint.hector.api.query.ColumnQuery;
-import me.prettyprint.hector.api.query.QueryResult;
+import me.prettyprint.cassandra.serializers.*;
+import me.prettyprint.cassandra.service.template.*;
+import me.prettyprint.hector.api.*;
+import me.prettyprint.hector.api.beans.*;
+import me.prettyprint.hector.api.exceptions.*;
+import me.prettyprint.hector.api.factory.*;
+import me.prettyprint.hector.api.mutation.*;
+import me.prettyprint.hector.api.query.*;
 
-import org.json.JSONArray;
-import org.json.JSONObject;
-import org.json.JSONException;
+import org.json.*;
 
 class CassInsertMulti {
 	private Keyspace keyspace;
@@ -37,25 +30,46 @@ class CassInsertMulti {
 			output = new JSONObject();
 			String where = (String)this.input.get("where");
 			JSONObject data = (JSONObject)this.input.get("data");
+			boolean is_super = false;
+			boolean is_counter = false;
+			if(this.input.has("is_counter"))
+				is_counter = this.input.getBoolean("is_counter");
 
 			try {
 				Mutator<String> mutator = HFactory.createMutator(keyspace, StringSerializer.get());
-			
 				Iterator it_data = data.keys();
 				while(it_data.hasNext()) {
-					Set<HColumn<String, String>> colums = new HashSet<HColumn<String,String>>();
-					String k_data = (String)it_data.next();
-					JSONObject v_data = (JSONObject)data.get(k_data);
-					Iterator it_inner = v_data.keys();
-					while(it_inner.hasNext()) {
-						String k_inner = (String)it_inner.next();
-						String v_inner = (String)v_data.get(k_inner);
-						colums.add(HFactory.createStringColumn(k_inner, v_inner));
+					if(is_super == false && is_counter == false) {
+						Set<HColumn<String, String>> colums = new HashSet<HColumn<String,String>>();
+						String k_data = (String)it_data.next();
+						JSONObject v_data = (JSONObject)data.get(k_data);
+						Iterator it_inner = v_data.keys();
+						while(it_inner.hasNext()) {
+							String k_inner = (String)it_inner.next();
+							String v_inner = v_data.get(k_inner).toString();
+							colums.add(HFactory.createStringColumn(k_inner, v_inner));
+						}
+						for(HColumn<String, String> column : colums) {
+							mutator.addInsertion(k_data, where, column);
+						}
+					} else if(is_super == false && is_counter == true) {
+						Set<HCounterColumn<String>> set = new HashSet<HCounterColumn<String>>();
+						String k_data = (String)it_data.next();
+						JSONObject v_data = (JSONObject)data.get(k_data);
+						Iterator it_inner = v_data.keys();
+						while(it_inner.hasNext()) {
+							String k_inner = (String)it_inner.next();
+							long v_inner = v_data.getLong(k_inner);
+							set.add(HFactory.createCounterColumn(k_inner, v_inner)); 
+						}
+						for(HCounterColumn<String> val : set) {
+							mutator.addCounter(k_data, where, val);
+						}
+					} else if(is_super == true && is_counter == true) {
+
 					}
-					for(HColumn<String, String> column : colums) {
-						mutator.addInsertion(k_data, where, column);
-					}
-				}
+
+				} 	
 				mutator.execute();
 			} catch(HectorException e) {
 				e.printStackTrace();
